c8a6d6d91411918746cddba3d91d98fab785f51d
Annemarie Kohler <annemarie.k89@gmail.com>
2016-05-04 10:36:22 +0100

    updated server test

--- test/features/setup.js
+++ test/features/setup.js
@@ -2,11 +2,10 @@ var request = require("request");
 var should = require("should");
 var expect = require("chai").expect;
 
-describe('Setup', function() {
-  it('should display hello world on the page', function(done) {
+describe('Server', function() {
+  it('should be up and running', function(done) {
     request.get('http://localhost:8080', function(err, response, body) {
       response.statusCode.should.equal(200);
-      expect(body).to.include("Hello World");
       done();
     });
   });
