323b0f01139af6b59382fc0272e866d4111bf16a
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-05-06 13:24:39 +0100

    Trying to get protractor test to pass

--- app/private/routes/index.js
+++ app/private/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var models = require('../app/private/server/models/index');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/events', function(req, res) {
+  console.log("Did the get route");
+  models.Events.findAll().then(function(events) {
+    res.send(events);
+  });
 });
 
 router.post('/events', function(req, res) {
--- app/public/javascripts/controllers/socialCalController.js
+++ app/public/javascripts/controllers/socialCalController.js
@@ -1,9 +1,15 @@
 angular
   .module('socialCal')
-  .controller('socialCalController', ['$http', 'socialCalPostService', function($http, socialCalPostService) {
+  .controller('socialCalController', ['$http', 'socialCalGetService', 'socialCalPostService', function($http, socialCalGetService, socialCalPostService) {
     var self = this;
 
     var events = [];
+
+    socialCalGetService.getEventsFromDB().then(function(events) {
+      return events.map(function(singleEvent) {
+        self.events.push(singleEvent);
+      });
+    });
 
     self.addEvent = function(eventTitle, eventDate, eventTime) {
       return socialCalPostService.postEventsToDB(eventTitle, eventDate, eventTime);
