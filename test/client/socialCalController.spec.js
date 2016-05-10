1352bfbbfae683a62ed5e1f68dd9ae166e710180
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-05-10 12:52:31 +0100

    Edited karma tests, now passing

--- test/client/socialCalController.spec.js
+++ test/client/socialCalController.spec.js
@@ -6,32 +6,34 @@ describe('socialCalController', function() {
   var title = 'title';
   var date = '13/05/2016';
   var time = '19:00';
-  var promise, res;
+  var promise;
 
   beforeEach(inject(function($controller, _socialCalPostService_, _$httpBackend_, _$cookies_) {
     ctrl = $controller('socialCalController');
     socialCalPostService = _socialCalPostService_;
     $httpBackend = _$httpBackend_;
     $cookies = _$cookies_;
-    promise = new Promise(function(resolve, reject) {
-      res = resolve;
+    resolvingPromise = new Promise(function(resolve, reject) {
+      resolve({status: 200});
     });
   }));
 
   describe('#addEvent', function() {
-    it('calls the socialCalPostService', function(done) {
-      spyOn(socialCalPostService, 'postEventsToDB').and.returnValue(promise);
-      expect(ctrl.addEvent()).toEqual(promise);
+    it('calls the socialCalPostService.postEventsToDB with title, date and time', function(done) {
+      spyOn(socialCalPostService, 'postEventsToDB').and.returnValue(resolvingPromise);
+      ctrl.addEvent(title, date, time);
+      expect(socialCalPostService.postEventsToDB).toHaveBeenCalledWith(title, date, time);
       done();
     });
   });
 
   describe('#signUpUser', function() {
-    it('creates a new user', function(done) {
+    it('calls the socialCalPostService.postUserToDB with username and password', function(done) {
       var username = "Test User";
       var password = "password";
-      spyOn(socialCalPostService, 'postUserToDB').and.returnValue(promise);
-      expect(ctrl.signUpUser(username,password)).toEqual(promise);
+      spyOn(socialCalPostService, 'postUserToDB').and.returnValue(resolvingPromise);
+      ctrl.signUpUser(username, password);
+      expect(socialCalPostService.postUserToDB).toHaveBeenCalledWith(username, password);
       done();
     });
   });
