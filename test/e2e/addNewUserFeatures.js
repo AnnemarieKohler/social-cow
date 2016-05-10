edfb5737970ac2376cecad79c6f9f3dbab15c6ca
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-05-10 11:46:34 +0100

    Protractor tests now passing

--- test/e2e/addNewUserFeatures.js
+++ test/e2e/addNewUserFeatures.js
@@ -1,15 +1,14 @@
 describe('socialCalendar', function() {
 
   it('signs up a new user', function() {
-    // var millisecs = 300000;
     browser.get('/');
+    $('#user-sign-out').click();
 
     $('#new-user-username').sendKeys("Test User");
     $('#new-user-password').sendKeys("password");
     $('#new-user-submit').click();
     browser.get('/');
     var userWelcome = $('#user-welcome').getText();
-    // console.log($$('#events ul li').last().getText());
     expect(userWelcome).toContain('Welcome, Test User');
   });
 
