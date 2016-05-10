cfd3156184a1a288644ebeef1ef9bccb062adfbc
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-05-10 11:24:48 +0100

    All tests passing for karma, need to double check

--- test/client/socialCalController.spec.js
+++ test/client/socialCalController.spec.js
@@ -1,23 +1,27 @@
 describe('socialCalController', function() {
-  beforeEach(module('socialCal', ['ngCookies']));
+  beforeEach(module('socialCal'));
 
   var ctrl, $httpBackend, socialCalPostService, $cookies;
 
   var title = 'title';
   var date = '13/05/2016';
   var time = '19:00';
+  var promise, res;
 
   beforeEach(inject(function($controller, _socialCalPostService_, _$httpBackend_, _$cookies_) {
-    ctrl = $controller('socialCalController', ['ngCookies']);
+    ctrl = $controller('socialCalController');
     socialCalPostService = _socialCalPostService_;
     $httpBackend = _$httpBackend_;
-    $cookies = _$cookies;
+    $cookies = _$cookies_;
+    promise = new Promise(function(resolve, reject) {
+      res = resolve;
+    });
   }));
 
   describe('#addEvent', function() {
     it('calls the socialCalPostService', function(done) {
-      spyOn(socialCalPostService, 'postEventsToDB').and.returnValue(200);
-      expect(ctrl.addEvent()).toEqual(200);
+      spyOn(socialCalPostService, 'postEventsToDB').and.returnValue(promise);
+      expect(ctrl.addEvent()).toEqual(promise);
       done();
     });
   });
@@ -26,8 +30,8 @@ describe('socialCalController', function() {
     it('creates a new user', function(done) {
       var username = "Test User";
       var password = "password";
-      spyOn(socialCalPostService, 'postUserToDB').and.returnValue(200);
-      expect(ctrl.signUpUser(username,password)).toEqual(200);
+      spyOn(socialCalPostService, 'postUserToDB').and.returnValue(promise);
+      expect(ctrl.signUpUser(username,password)).toEqual(promise);
       done();
     });
   });
