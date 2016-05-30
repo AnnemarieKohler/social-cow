13a719a79b67dce737777b23496dd6b2fe02900d
Anne <annemarie.k89@gmail.com>
2016-05-30 18:12:00 +0100

    fixed mocha tests

--- test/server/addEventToDB.js
+++ test/server/addEventToDB.js
@@ -4,32 +4,28 @@ var expect = require("chai").expect;
 var models = require('../../app/private/server/models/index');
 var Sequelize = require('sequelize');
 
-before(function(done) {
-  models.Events.sync({ force : true }) // drops table and re-creates it
+beforeEach(function(done) {
+  models.Event.sync({ force : true }) // drops table and re-creates it
     .then(function() {
       done(null);
     })
-    .catch(function() {
-      console.log("Error");
+    .catch(function(err) {
+      console.log("Failed", err);
     });
 });
 
 describe('Adding to DB', function() {
-
-  it('should add an event to the database', function(done) {
-    var newEvent = {title: 'New Event',
-                    date: '13/05/2016',
-                    time: '19:00'}
-    models.Events.findOrCreate({
-      where: newEvent
-    }).then(function() {
-      models.Events.findAll().then(function(events) {
+  it('adds event to the database', function(done) {
+    var newEvent = { title: 'New Event', date: '13/05/2016',time: '19:00' }
+    models.Event
+      .create(newEvent)
+      .then(function() {
+      models.Event.findAll().then(function(events) {
         expect(events.length).to.equal(1);
         done();
       });
-    }).catch(function() {
-      console.log("Failed");
+    }).catch(function(err) {
+      console.log("Failed", err);
     });
-
   });
 });
--- test/server/addUserToDB.js
+++ test/server/addUserToDB.js
@@ -4,33 +4,28 @@ var expect = require("chai").expect;
 var models = require('../../app/private/server/models/index');
 var Sequelize = require('sequelize');
 
-before(function(done) {
-  models.Users.sync({ force : true }) // drops table and re-creates it
+beforeEach(function(done) {
+  models.User.sync({ force : true }) // drops table and re-creates it
     .then(function() {
       done(null);
     })
-    .catch(function() {
-      console.log("Error");
+    .catch(function(err) {
+      console.log("Failed", err);
     });
 });
 
 describe('Adding user to DB', function() {
 
   it('should sign up a user to the database', function(done) {
-    var newUser = {
-                    username: "Test User",
-                    password: "password"
-                  };
+    var newUser = { username: "Test User", password: "password" };
 
-    models.Users.findOrCreate({
-      where: newUser
-    }).then(function() {
-      models.Users.findAll().then(function(users) {
+    models.User.create(newUser).then(function() {
+      models.User.findAll().then(function(users) {
         expect(users.length).to.equal(1);
         done();
       });
-    }).catch(function() {
-      console.log("Failed");
+    }).catch(function(err) {
+      console.log("Failed", err);
     });
 
   });
