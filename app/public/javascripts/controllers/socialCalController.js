6526976cdcdfb256e63a2496a70224cb718fde6e
Anne <annemarie.k89@gmail.com>
2016-05-30 14:24:30 +0100

    refactored the formatation of the time in two separate functions. one for formatting the date coming from the DB and the other for sending it to the DB.

--- app/public/javascripts/controllers/socialCalController.js
+++ app/public/javascripts/controllers/socialCalController.js
@@ -1,97 +1,104 @@
 angular
   .module('socialCal')
   .controller('socialCalController', ['alert', '$window', '$cookies', '$http', 'socialCalGetService', 'socialCalPostService', 'userPersistenceService',
-                                      function(alert, $window, $cookies, $http, socialCalGetService, socialCalPostService, userPersistenceService) {
+    function(alert, $window, $cookies, $http,
+             socialCalGetService, socialCalPostService,
+             userPersistenceService) {
 
-    var self = this;
+      var self = this;
 
-    self.user = userPersistenceService.getCookieData();
+      self.user = userPersistenceService.getCookieData();
 
-    self.checkboxModel = {
-      value1 : "YES"
-    };
-    self.events = [];
-    self.eventSources = [{
-      title: "PUB PLEASE",
-      start: "2016-05-06T19:00",
-      end: "2016-05-06T21:00"
-    }];
+      self.checkboxModel = {
+        value1 : "YES"
+      };
+      self.eventSources = [];
 
-    socialCalGetService.getEventsFromDB().then(function(events) {
-      return events.map(function(singleEvent) {
-        var correctDateFormat = moment(singleEvent.date.replace("00:00:00", singleEvent.time)).format('YYYY-MM-DDTHH:mm');
-        var dateTime = moment(correctDateFormat).subtract(1, 'hours');
-        return self.eventSources.push({
-          title: singleEvent.title,
-          start: dateTime
+      socialCalGetService.getEventsFromDB().then(function(events) {
+        return events.map(function(singleEvent) {
+          return self.eventSources.push({
+            title: singleEvent.title,
+            start: _formatTimeFromDBFormat(singleEvent.date, singleEvent.time)
+          });
         });
       });
-    });
 
-    self.signUpUser = function(username, password) {
-      userPersistenceService.setCookieData(username);
-      return socialCalPostService.postUserToDB(username, password).then(function(response) {
-        if(response.status === 200){
-          $window.location.reload();
-        } else {
-          console.log(response.status);
+      self.addEvent = function(eventTitle, eventDate, eventTime) {
+        return socialCalPostService.postEventsToDB(eventTitle, eventDate, eventTime)
+        .then(function(response) {
+          if(response.status === 200){
+            self.eventSources.push({
+              title: eventTitle,
+              start: _formatTimeToDBFormat(eventTime, eventDate)
+            });
+          }
+        })
+        .catch(function(){
+          console.log("Failed");
+        });
+      };
+
+      self.signUpUser = function(username, password) {
+        userPersistenceService.setCookieData(username);
+        return socialCalPostService.postUserToDB(username, password).then(function(response) {
+          if(response.status === 200){
+            $window.location.reload();
+          } else {
+            console.log(response.status);
+          }
+        });
+      };
+
+      self.signInUser = function(username, password) {
+        return socialCalPostService.validateUserInDB(username, password).then(function(response) {
+          if(response.data.length === 0){
+            alert("NO");
+          } else {
+            userPersistenceService.setCookieData(username);
+            $window.location.reload();
+          }
+        });
+      };
+
+      self.signOutUser = function() {
+        userPersistenceService.clearCookieData();
+      };
+
+      self.alertOnEventClick = function(date, jsEvent, view) {
+        alert.show('Clicked', date);
+        $('#calendar').fullCalendar('updateEvent', date);
+      };
+
+      self.uiConfig = {
+        calendar:{
+          height: 450,
+          editable: true,
+          header:{
+            left: 'month basicWeek basicDay agendaWeek agendaDay',
+            center: 'title',
+            right: 'today prev,next'
+          },
+          timeFormat: "H:mm",
+          eventClick: self.alertOnEventClick,
+          eventDrop: self.alertOnDrop,
+          eventResize: self.alertOnResize,
+          events: self.eventSources
         }
-      });
-    };
+      };
 
-    self.signInUser = function(username, password) {
-      return socialCalPostService.validateUserInDB(username, password).then(function(response) {
-        if(response.data.length === 0){
-          alert("NO");
-        } else {
-          userPersistenceService.setCookieData(username);
-          $window.location.reload();
-        }
-      });
-    };
+      function _formatTimeFromDBFormat(date, time) {
+        var formattedDate = moment(date.replace("00:00:00", time))
+                                       .format('YYYY-MM-DDTHH:mm');
+        return moment(formattedDate).subtract(1, 'hours');
+      };
 
-    self.signOutUser = function() {
-      userPersistenceService.clearCookieData();
-    };
+      function _formatTimeToDBFormat(eventTime, eventDate) {
+        var dateArray = eventTime.toString().split(" ");
+        var time = dateArray[4];
 
-    self.addEvent = function(eventTitle, eventDate, eventTime) {
-      return socialCalPostService.postEventsToDB(eventTitle, eventDate, eventTime)
-      .then(function(response) {
-        if(response.status === 200){
-          var dateArray = eventTime.toString().split(" ");
-          var correctTimeFormat = dateArray[4];
-          var dateTime = moment(eventDate.toString().replace("00:00:00", correctTimeFormat)).format('YYYY-MM-DDTHH:mm');
-          self.eventSources.push({
-            title: eventTitle,
-            start: dateTime
-          });
-        }
-      })
-      .catch(function(){
-        console.log("Failed");
-      });
-    };
-
-    self.alertOnEventClick = function(date, jsEvent, view) {
-      alert.show('Clicked', date);
-      $('#calendar').fullCalendar('updateEvent', date);
-    };
-
-    self.uiConfig = {
-      calendar:{
-        height: 450,
-        editable: true,
-        header:{
-          left: 'month basicWeek basicDay agendaWeek agendaDay',
-          center: 'title',
-          right: 'today prev,next'
-        },
-        timeFormat: "H:mm",
-        eventClick: self.alertOnEventClick,
-        eventDrop: self.alertOnDrop,
-        eventResize: self.alertOnResize,
-        events: self.eventSources
-      }
-    };
+        return moment(eventDate.toString()
+                               .replace("00:00:00", time))
+                               .format('YYYY-MM-DDTHH:mm');
+      };
 
   }]);
