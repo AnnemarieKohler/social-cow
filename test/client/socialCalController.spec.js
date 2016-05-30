d2a3c26d9188879e31488a974b60beb49e2b42f0
Anne <annemarie.k89@gmail.com>
2016-05-30 14:23:43 +0100

    checks for empty eventSources when initialised

--- test/client/socialCalController.spec.js
+++ test/client/socialCalController.spec.js
@@ -8,33 +8,46 @@ describe('socialCalController', function() {
   var time = '19:00';
   var resolvingPromise;
 
-  beforeEach(inject(function($controller, _socialCalPostService_, _$httpBackend_, _$cookies_) {
-    ctrl = $controller('socialCalController');
-    socialCalPostService = _socialCalPostService_;
-    $httpBackend = _$httpBackend_;
-    $cookies = _$cookies_;
-    resolvingPromise = new Promise(function(resolve, reject) {
-      resolve({status: 200});
-    });
+  beforeEach(
+    inject(function($controller, _socialCalPostService_,
+                    _$httpBackend_, _$cookies_) {
+      ctrl = $controller('socialCalController');
+      socialCalPostService = _socialCalPostService_;
+      $httpBackend = _$httpBackend_;
+      $cookies = _$cookies_;
+      resolvingPromise = new Promise(function(resolve, reject) {
+        resolve({status: 200});
+      });
   }));
 
-  describe('#addEvent', function() {
-    it('calls the socialCalPostService.postEventsToDB with title, date and time', function(done) {
+  describe('.eventSources', function() {
+    it('initialises with an empty array for the eventSources', function() {
+        expect(ctrl.eventSources).toEqual([]);
+    });
+  });
+
+  describe('.addEvent', function() {
+    it('calls the socialCalPostService.postEventsToDB with title,date and time', function(done) {
       spyOn(socialCalPostService, 'postEventsToDB').and.returnValue(resolvingPromise);
       ctrl.addEvent(title, date, time);
-      expect(socialCalPostService.postEventsToDB).toHaveBeenCalledWith(title, date, time);
+      expect(socialCalPostService.postEventsToDB)
+        .toHaveBeenCalledWith(title, date, time);
       done();
     });
   });
 
-  describe('#signUpUser', function() {
+  describe('.signUpUser', function() {
     it('calls the socialCalPostService.postUserToDB with username and password', function(done) {
       var username = "Test User";
       var password = "password";
-      spyOn(socialCalPostService, 'postUserToDB').and.returnValue(resolvingPromise);
+      spyOn(socialCalPostService,
+        'postUserToDB').and.returnValue(resolvingPromise);
       ctrl.signUpUser(username, password);
-      expect(socialCalPostService.postUserToDB).toHaveBeenCalledWith(username, password);
+      expect(socialCalPostService.postUserToDB)
+        .toHaveBeenCalledWith(username, password);
       done();
     });
   });
+
+
 });
